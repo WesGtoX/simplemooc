37ecb02081fa865875ad4cda3d37da97eea83d3a
Wesley Mendes <wesley_gto_x@yahoo.com.br>
2018-05-10 00:39:16 -0300

    Update templates

--- simplemooc/core/views.py
+++ simplemooc/core/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-	return HttpResponse('Hello World')
+	return render(request, 'home.html', {'usuario': 'Fulano de Tal'})
+	# 1º parâmetro é o 'request'
+	# 2º parâmetro 'home.html', é o nome do template
+	# 3º parâmetro passei um dicionário que são as variaveis que ficaram disponiveis no template.
