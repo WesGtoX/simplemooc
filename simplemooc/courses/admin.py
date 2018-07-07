7b468d4a7718aa6508cb3abaa86f035dd6ab19ba
Wesley Mendes <wesley_gto_x@yahoo.com.br>
2018-07-07 18:55:17 -0300

    simplemooc v1.4.7 class admin

--- simplemooc/courses/admin.py
+++ simplemooc/courses/admin.py
@@ -1,12 +1,30 @@
 from django.contrib import admin
 
-from .models import Course, Enrollment, Announcement, Comment
+from .models import (Course, Enrollment, Announcement, Comment, Lesson, Material)
+
 
 class CourseAdmin(admin.ModelAdmin):		# representa as opções do curso de uma forma melhor, com mais opções.
 	
-	list_display = ['name', 'slug', 'start_date', 'created_at']
-	search_fields = ['name', 'slug']
+	list_display = ['name', 'slug', 'start_date', 'created_at']		# o que será mostrado.
+	search_fields = ['name', 'slug']		# campo de busca.
 	prepopulated_fields = {'slug': ('name',)}	# preenche automaticamente com a devida formatação o campo 'slug'.
 
+
+class MaterialInlineAdmin(admin.StackedInline):		# 'InlineModelAdmin', pode ser escolhido duas formas de exibição 'StackedInline' (vertical) e o 'TabularInline' (horizontal).
+
+	model = Material 		# model indicado que será tratado como 'InlineModelAdmin'.
+
+
+class LessonAdmin(admin.ModelAdmin):
+
+	list_display = ['name', 'number', 'course', 'release_date']		# o que será mostrado.
+	search_fields = ['name', 'description']		# campo de busca.
+	list_filter = ['created_at']		# filtragem lateral. Cria um campo de filtro lateral no admin.
+
+	inlines = [		# variável 'inlines' que é uma lista.
+		MaterialInlineAdmin
+	]
+
 admin.site.register(Course, CourseAdmin)
-admin.site.register([Enrollment, Announcement, Comment])
+admin.site.register([Enrollment, Announcement, Comment, Material])
+admin.site.register(Lesson, LessonAdmin)
