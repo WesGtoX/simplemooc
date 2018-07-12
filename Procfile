a76ed63550522547f5fcbb6d7fc0ec3c54351abc
Wesley Mendes <wesley_gto_x@yahoo.com.br>
2018-07-12 02:42:42 -0300

    simplemooc v1.5.0 deploy in heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn simplemooc.wsgi
--- simplemooc/wsgi.py
+++ simplemooc/wsgi.py
@@ -13,4 +13,5 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "simplemooc.settings")
 
-application = get_wsgi_application()
+from dj_static import Cling
+application = Cling(get_wsgi_application())
