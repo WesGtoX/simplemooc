4ce13f26c07df2ee26d72743ff7c68cffa1db546
Wesley Mendes <wesley_gto_x@yahoo.com.br>
2018-07-12 17:53:27 -0300

    simplemooc v1.5.0 deploy in heroku change settings static test whitenoise 3

--- simplemooc/settings.py
+++ simplemooc/settings.py
@@ -172,7 +172,7 @@ ALLOWED_HOSTS = ['*']
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
