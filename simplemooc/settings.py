8f50fc863143d44bc352abd8563945f1de23c270
Wesley Mendes <wesley_gto_x@yahoo.com.br>
2018-07-12 03:47:25 -0300

    simplemooc v1.5.0 deploy in heroku fixes bug staticfiles_dir

--- simplemooc/settings.py
+++ simplemooc/settings.py
@@ -164,9 +164,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+STATICFILES_DIRS = [
+    os.path.join(PROJECT_ROOT, 'static'),
+]
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
