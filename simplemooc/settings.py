623655cf7723bc8cb972d3503603d8857e0abe5d
Wesley Mendes <wesley_gto_x@yahoo.com.br>
2018-07-12 17:49:11 -0300

    simplemooc v1.5.0 deploy in heroku change settings static test whitenoise

--- simplemooc/settings.py
+++ simplemooc/settings.py
@@ -182,7 +182,7 @@ STATIC_URL = '/static/'
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
-#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Activate Django-Heroku.
 #import django_heroku
