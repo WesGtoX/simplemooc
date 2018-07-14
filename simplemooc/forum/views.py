d6e974cc997bce79dd35f193123cd3c69072342a
Wesley Mendes <wesley_gto_x@yahoo.com.br>
2018-07-14 01:03:52 -0300

    forum modeling, class-based views in forum part 2

--- simplemooc/forum/views.py
+++ simplemooc/forum/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 
 
-class ForumView(TemplateName):
+class ForumView(TemplateView):
 
 	template_name = 'forum/index.html'
+
+index = ForumView.as_view()		# 'index' recebe o resultado de 'ForumView.as_view()' o 'as_view' retorna uma função.
+
+#index = TemplateView.as_view(template_name='forum/index.html')		# também poderia ter sido declarada dessa forma.
+
--- simplemooc/urls.py
+++ simplemooc/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
 	path('', include('simplemooc.core.urls', namespace='core')),
 	path('conta/', include('simplemooc.accounts.urls', namespace='accounts')),
 	path('cursos/', include('simplemooc.courses.urls', namespace='courses')),
+	path('forum/', include('simplemooc.forum.urls', namespace='forum')),
     path('admin/', admin.site.urls),
 ]
 
