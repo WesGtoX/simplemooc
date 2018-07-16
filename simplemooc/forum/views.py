d5c25b2b39b0e7d0159d9abce74a092ceb401e45
Wesley Mendes <wesley_gto_x@yahoo.com.br>
2018-07-16 19:09:37 -0300

    listing of topics 2, 3 and by tag, local_settings_git

--- simplemooc/forum/views.py
+++ simplemooc/forum/views.py
@@ -15,9 +15,24 @@ from .models import Thread
 #	#template_name = 'forum/index.html'
 class ForumView(ListView):
 
-	model = Thread
-	paginate_by = 10
+	#model = Thread		# na listagem indicamos o model, mas podemos indicar a 'queryset'.
+	paginate_by = 2
 	template_name='forum/index.html'
+
+	def get_queryset(self):		# uma das opções de customização do 'ListView'.
+		queryset = Thread.objects.all()		# quando indica o 'model', ele faz isso 'Thread.objects.all()'.
+		#if 'order' in self.request.GET:		# procura o parâmetro, . Quando faço uma view baseada em função, o objeto request sempre estará disponível na função, quando faço em classe, esse objeto estará disponível na instância.
+		order = self.request.GET.get('order', '')		# pode ser que o parâmetro 'order' não exista, então pode dar erro, então a string vazia ('') é o valor default.
+		if order == 'views':
+			queryset = queryset.order_by('-views')
+		elif order == 'answers':
+			queryset =  queryset.order_by('-answers')
+		return queryset
+
+	def get_context_data(self, **kwargs):
+	    context = super(ForumView, self).get_context_data(**kwargs)		# 'super' pegar os '**kwargs' e joga no 'contexto', esse '**kwargs' são argumentos que passa na 'url'.
+	    context['tags'] = Thread.tags.all()		# não estou chamando uma instancia, quando chamo uma instancia, o tags é filtrado pelos valores que aquela instancia tem, quando chamo pela classe, ele chama todos as tags associadas a alguma instancia dessa classe.
+	    return context
 
 
 index = ForumView.as_view()		# 'index' recebe o resultado de 'ForumView.as_view()' o 'as_view' retorna uma função.
