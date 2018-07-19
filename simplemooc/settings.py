830f6803b5c3f379ad9e769ab41b1f6f9174c2bf
Wesley Mendes <wesley_gto_x@yahoo.com.br>
2018-07-19 12:23:30 -0300

    update settings static files

--- simplemooc/settings.py
+++ simplemooc/settings.py
@@ -180,9 +180,9 @@ ALLOWED_HOSTS = ['*']
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
-)
+STATICFILES_DIRS = [
+    os.path.join(PROJECT_ROOT, 'static'),
+]
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
