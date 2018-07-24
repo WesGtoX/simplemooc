b0cc081628ec9c4b39955dc153a840bbd6a61308
Wesley Mendes <wesley_gto_x@yahoo.com.br>
2018-07-24 19:44:16 -0300

    update settings, aws s3 staticfiles

--- simplemooc/settings.py
+++ simplemooc/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # libs.
     'taggit',
+    'storages',
     #apps
     # 'path' da aplicação (movendo o 'app' para pasta 'simplemooc', pode ser referenciada dessa maneira).
     'simplemooc.core',
@@ -187,11 +188,30 @@ STATIC_URL = '/static/'
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Activate Django-Heroku.
-import django_heroku
-django_heroku.settings(locals())
+#import django_heroku
+#django_heroku.settings(locals())
+
+
+# AWS S3 SETTINGS
+
+AWS_STORAGE_BUCKET_NAME = os.environ['s3_bucket_name']
+AWS_S3_REGION_NAME = os.environ['s3_region_name']
+AWS_ACCESS_KEY_ID = os.environ['s3_key_id']
+AWS_SECRET_ACCESS_KEY = os.environ['s3_secret_key']
+
+# Tell django-storages the domain to use to refer to static files.
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+
+# Tell the staticfiles app to use S3Boto3 storage when writing the collected static files (when you run `collectstatic`).
+STATICFILES_LOCATION = 'static'
+STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+
+MEDIAFILES_LOCATION = 'media'
+DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
+
 
 try:
     from simplemooc.local_settings import *
